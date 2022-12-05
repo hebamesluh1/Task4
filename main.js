59a87284dc848031b5615d39005fa87a2091c0df
hebamesluh1 <hebamesluh@gmail.com>
2022-12-05 19:47:27 +0200

    add js file

---
+++ main.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('button');
+let txtColor = document.querySelector('#text-color');
+let paint = document.querySelector('#paint-me');
+
+btn.addEventListener('click',()=>{
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    txtColor.value = paint.style.backgroundColor = "#" + randomColor;}
+    );
+txtColor.addEventListener('input',()=>{
+    paint.style.backgroundColor=txtColor.value;
+});
