c04261edbc99d255c57e219d55431ba289505667
hebamesluh1 <hebamesluh@gmail.com>
2022-12-05 20:26:23 +0200

    update

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let txtColor = document.querySelector('#text-color');
 let paint = document.querySelector('#paint-me');
 
 btn.addEventListener('click',()=>{
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    txtColor.value = paint.style.backgroundColor = "#" + randomColor;}
+    let randomColor = "#"+Math.floor(Math.random() * 16777215).toString(16);
+    txtColor.value = paint.style.backgroundColor =randomColor;}
     );
 txtColor.addEventListener('input',()=>{
     paint.style.backgroundColor=txtColor.value;
