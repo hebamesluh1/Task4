58e600cbd79e38c78869e4ebc2846bb7968f350b
hebamesluh1 <hebamesluh@gmail.com>
2022-12-05 21:21:15 +0200

    add mouseover

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ let btn = document.querySelector('button');
 let txtColor = document.querySelector('#text-color');
 let paint = document.querySelector('#paint-me');
 
-btn.addEventListener('click',()=>{
+// create random color 
+const randomColor =()=>{
     let randomColor = "#"+Math.floor(Math.random() * 16777215).toString(16);
-    txtColor.value = paint.style.backgroundColor =randomColor;}
-    );
+    txtColor.value = paint.style.backgroundColor =randomColor;
+}
+btn.addEventListener('click',randomColor);
+
+
+//change color by input box
 txtColor.addEventListener('input',()=>{
     paint.style.backgroundColor=txtColor.value;
+});
+
+
+// mouse hover and leave changes color
+let Timer;
+paint.addEventListener('mouseover',()=>{
+    Timer = setInterval(randomColor,(500));
+    
+});
+paint.addEventListener('mouseleave',()=>{
+    clearInterval(Timer);
 });
